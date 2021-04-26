f81592ae78b64e8d5fafc98f788375341e0a285f
Natalia609 <74922133+Natalia609@users.noreply.github.com>
2021-04-26 15:46:22 +0300

    Add files via upload

--- flsite.py
+++ flsite.py
@@ -16,7 +16,7 @@ MAX_CONTENT_LENGTH = 1024 * 1024
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config.update(dict(DATABASE=os.path.join(app.root_path,'flsite.db')))
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
 app.register_blueprint(admin, url_prefix='/admin')
 
@@ -31,10 +31,12 @@ def load_user(user_id):
     print("load_user")
     return UserLogin().fromDB(user_id, dbase)
 
+
 def connect_db():
     conn = sqlite3.connect(app.config['DATABASE'])
     conn.row_factory = sqlite3.Row
     return conn
+
 
 def create_db():
     """Вспомогательная функция для создания таблиц БД"""
@@ -44,6 +46,7 @@ def create_db():
     db.commit()
     db.close()
 
+
 def get_db():
     '''Соединение с БД, если оно еще не установлено'''
     if not hasattr(g, 'link_db'):
@@ -52,6 +55,8 @@ def get_db():
 
 
 dbase = None
+
+
 @app.before_request
 def before_request():
     """Установление соединения с БД перед выполнением запроса"""
@@ -71,19 +76,20 @@ def close_db(error):
 def index():
     return render_template('index.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
 
+
 @app.route("/add_post", methods=["POST", "GET"])
 def addPost():
     if request.method == "POST":
         if len(request.form['name']) > 4 and len(request.form['post']) > 10:
             res = dbase.addPost(request.form['name'], request.form['post'], request.form['url'])
             if not res:
-                flash('Ошибка добавления статьи', category = 'error')
+                flash('Ошибка добавления статьи', category='error')
             else:
                 flash('Статья добавлена успешно', category='success')
         else:
             flash('Ошибка добавления статьи', category='error')
 
-    return render_template('add_post.html', menu = dbase.getMenu(), title="Добавление статьи")
+    return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
 
 
 @app.route("/post/<alias>")
@@ -94,6 +100,7 @@ def showPost(alias):
         abort(404)
 
     return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -118,13 +125,13 @@ def login():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-            hash = generate_password_hash(form.psw.data)
-            res = dbase.addUser(form.name.data, form.email.data, hash)
-            if res:
-                flash("Вы успешно зарегистрированы", "success")
-                return redirect(url_for('login'))
-            else:
-                flash("Ошибка при добавлении в БД", "error")
+        hash = generate_password_hash(form.psw.data)
+        res = dbase.addUser(form.name.data, form.email.data, hash)
+        if res:
+            flash("Вы успешно зарегистрированы", "success")
+            return redirect(url_for('login'))
+        else:
+            flash("Ошибка при добавлении в БД", "error")
 
     return render_template("register.html", menu=dbase.getMenu(), title="Регистрация", form=form)
 
@@ -135,6 +142,7 @@ def logout():
     logout_user()
     flash("Вы вышли из аккаунта", "success")
     return redirect(url_for('login'))
+
 
 @app.route('/profile')
 @login_required
@@ -153,6 +161,7 @@ def userava():
     h.headers['Content-Type'] = 'image/png'
     return h
 
+
 @app.route('/upload', methods=["POST", "GET"])
 @login_required
 def upload():
@@ -163,7 +172,7 @@ def upload():
                 img = file.read()
                 res = dbase.updateUserAvatar(img, current_user.get_id())
                 if not res:
-                    flash("Ошибка обновления аватара", "error")
+                    flash("Ошибка обновлен';;;;;;;ия аватара", "error")
                 flash("Аватар обновлен", "success")
             except FileNotFoundError as e:
                 flash("Ошибка чтения файла", "error")
@@ -171,6 +180,7 @@ def upload():
             flash("Ошибка обновления аватара", "error")
 
     return redirect(url_for('profile'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
